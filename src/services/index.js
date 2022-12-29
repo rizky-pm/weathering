e591d6891db305468c92f431c829d7ddb56da843
Rizky Putra Mahendra <rizkymahendra2346@gmail.com>
2022-12-29 10:26:25 +0700

    removed unused function

--- src/services/index.js
+++ src/services/index.js
@@ -1,26 +1,5 @@
 import api from '../config/axios.config';
 
-export const getCurrentWeather = async ({ queryKey }) => {
-  const location = queryKey[1];
-
-  if (!location) {
-    return null;
-  }
-
-  const response = await api.get(`/current.json`, {
-    params: {
-      q: location,
-      aqi: 'no',
-    },
-  });
-
-  if (response.statusText !== 'OK') {
-    throw new Error(`Something went wrong with the API call.`);
-  }
-
-  return response;
-};
-
 export const getCurrentForecast = async ({ queryKey }) => {
   const location = queryKey[1];
 
