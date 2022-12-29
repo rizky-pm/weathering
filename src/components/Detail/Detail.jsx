7f605d235cb54283ac218cdf2637dfb346c6e7f7
Rizky Putra Mahendra <rizkymahendra2346@gmail.com>
2022-12-29 11:45:22 +0700

    Added weatherapi watermark

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -60,6 +60,13 @@ const Detail = ({ astroData, data }) => {
             </p>
           </div>
         </div>
+        <a href='https://www.weatherapi.com/' title='Free Weather API'>
+          <img
+            src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png'
+            alt='Weather data by WeatherAPI.com'
+            border='0'
+          />
+        </a>
       </div>
     </DetailStyled>
   );
